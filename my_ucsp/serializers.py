80bbe29521a280e64b12427714bfe6bb39d4fa6b
Mathortas <elderiva83@gmail.com>
2025-06-26 22:42:37 -0500

    Update serializers.py

--- my_ucsp/serializers.py
+++ my_ucsp/serializers.py
@@ -1,22 +1,41 @@
 from rest_framework import serializers
-from .models import Curso, Tarea, Nota, Matricula
+from .models import (
+    Curso, Horario, Matricula, Tarea, Nota, CategoriaPrincipal
+)
+
+class CategoriaPrincipalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CategoriaPrincipal
+        fields = ['id_categoria', 'nombre']
 
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Curso
-        fields = '__all__'
+        fields = ['id_curso', 'nombre', 'ciclo', 'modalidad', 'descripcion', 'creditos']
 
-class TareaSerializer(serializers.ModelSerializer):
+class HorarioSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Tarea
-        fields = '__all__'
-
-class NotaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Nota
-        fields = '__all__'
+        model = Horario
+        fields = ['id_horario', 'id_curso', 'dia_semana', 'hora_inicio', 'hora_fin', 'aula']
 
 class MatriculaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Matricula
-        fields = '__all__'
+        fields = ['id_matricula', 'id_usuario', 'id_curso', 'ciclo', 'fecha_matricula']
+
+class TareaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tarea
+        fields = [
+            'id_tarea', 'nombre_tarea', 'descripcion', 'fecha_entrega',
+            'estado', 'id_usuario', 'id_curso',
+            'id_categoria', 'peso_porcentaje'
+        ]
+
+class NotaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Nota
+        fields = [
+            'id_nota', 'id_categoria', 'id_tarea', 'id_matricula',
+            'nombre_nota', 'nota_obtenida', 'peso_porcentaje'
+        ]
