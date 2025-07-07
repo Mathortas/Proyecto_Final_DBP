a1f31f9aa4b41383e6e88936fd141bc7baf15f08
Mathortas <elderiva83@gmail.com>
2025-07-07 02:27:05 -0500

    Update api_views.py para visualizar tareas propias en app

--- my_ucsp/api_views.py
+++ my_ucsp/api_views.py
@@ -30,9 +30,23 @@ class MatriculaViewSet(viewsets.ModelViewSet):
         return Matricula.objects.filter(id_usuario=self.request.user)
 
 class TareaViewSet(viewsets.ModelViewSet):
-    queryset = Tarea.objects.all()
     serializer_class = TareaSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_authenticated:
+            return Tarea.objects.filter(id_usuario=user)
+        # Si la web necesita mostrar todas cuando no hay autenticación
+        return Tarea.objects.all()
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        if user.is_authenticated:
+            serializer.save(id_usuario=user)
+        else:
+            # Si quieres evitar que usuarios anónimos creen
+            raise PermissionDenied("Debe autenticarse para crear tareas.")
 
 class NotaViewSet(viewsets.ModelViewSet):
     queryset = Nota.objects.all()
